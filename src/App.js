296014074380032e0b0e41dc219d6b3a8baa8e9b
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2023-01-02 12:54:10 +1300

    Updated footer

--- src/App.js
+++ src/App.js
@@ -10,7 +10,24 @@ export default function App() {
           <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Olga Sergiienko</small>
+          <small>
+            Coded by Olga Sergiienko and is{" "}
+            <a
+              href="https://github.com/igorivnaolga/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on Github
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://dictionary-reactapp-igorivna.netlify.app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              hosted on Netlify
+            </a>
+          </small>
         </footer>
       </div>
     </div>
